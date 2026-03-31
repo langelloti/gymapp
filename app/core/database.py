12a0549cc581349e0c94f71ccbd35fe17bdd586f
Lucas <lucasangelloti@gmail.com>
2026-03-31 17:33:22 -0300

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,3 +1,18 @@
-import os
+﻿import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://postgres:271117@localhost:5433/gymapp"
+)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+Base = declarative_base()
