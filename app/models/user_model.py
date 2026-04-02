530b0b4b768a9586fb0d7b26ce15abbae61f41c8
Lucas <lucasangelloti@gmail.com>
2026-04-02 11:00:09 -0300

    aaa

---
+++ app/models/user_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from core.database import Base
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True, index=True)
+    senha = Column(String, nullable=False)
